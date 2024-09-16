8f04debc042e4bb3b6b89c8b0a39ab49167d1e24
Daher Maluf <daher.maluf@banhaia.com.ar>
2024-09-15 23:30:45 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TranslocoPipe, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TranslocoApp';
+  currentLang: string;
+
+  constructor(private translocoService: TranslocoService) {
+    this.currentLang = this.translocoService.getActiveLang();
+    this.translocoService.langChanges$.subscribe(
+      (lang: string) => this.currentLang = lang
+    );
+  }
+
+  changeLanguage(lang: string) {
+    this.translocoService.setActiveLang(lang);
+  }
 }
